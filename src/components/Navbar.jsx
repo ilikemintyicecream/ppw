af501741fc0e325122c0dd4d6ce109902d516561
deen <ilikemintyicecream@gmail.com>
2023-04-26 12:58:54 +0530

    made navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,63 @@
-import { NavLink } from "react-router-dom";
+import React, { useState } from 'react';
+import { AiOutlineHome, AiOutlineUser, AiOutlinePhone} from "react-icons/ai";
+import {BiPaint} from 'react-icons/bi'
+import{FaBars, FaTimes} from 'react-icons/fa'
+
+import './nav.css'
 
 const Navbar = () => {
-    return (
-<nav>
-    <ul>
-        <li>
-            <NavLink to='/'>Home</NavLink>
-        </li>
-    </ul>
-    <ul>
-        <li>
-            <NavLink to = '/about'>About</NavLink>
-        </li>
-    </ul>
-    <ul>
-        <li>
-            <NavLink to = '/Contact'>Contact</NavLink>
-        </li>
-    </ul>
-</nav>
+  const style = { color: "white", fontSize: "1.5em"}
 
-    )
-}
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle=()=> {
+    setToggle(!toggle);
+  }
+
+  
+return(<>
+
+ 
+ <nav className={toggle? 'navbarExpanded' : 'NavbarItems'}>
+ <h1 className='logo'>logo</h1>
+
+
+ <ul className='links' >
+
+  <li>
+    
+    <a className='NavLinks'  href="/">< AiOutlineHome/><div className='NavText'>Home</div></a>
+  </li>
+ </ul>
+ <ul>
+  <li>
+    
+    <a className='NavLinks' href="/About">< AiOutlineUser/><div className='NavText'>About</div></a>
+  </li>
+ </ul>
+ <ul>
+  <li>
+    
+    <a className='NavLinks' href="/Hobbies"><BiPaint/><div className='NavText'>Hobbies</div></a>
+  </li>
+ </ul>
+ <ul>
+  <li>
+    
+    <a className='NavLinks' href="/Contact">< AiOutlinePhone/><div className='NavText'>Contact</div></a>
+  </li>
+  
+ </ul>
+
+ 
+ <div className='toggle-icon' onClick={handleToggle}>
+  {toggle ? <FaTimes/>:<FaBars/>}
+ </div>
+
+ 
+ </nav>
+ </>
+
+)}
 
 export default Navbar;
