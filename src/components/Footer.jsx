68c154e9567b13bb6a0b5e8f717f1a394c09c2b9
deen <ilikemintyicecream@gmail.com>
2023-05-02 14:40:48 +0530

    added navbar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,44 @@
-
+import './footer.css'
 
 const Footer = () => {
     return (
     
-        <h1></h1>
+<div className="footer-basic">
+<footer>
+   
+  
+
+<ul className='list-inline' >
+
+<li className="list-inline-item">
+  <a href="/"><div>Home</div></a>
+</li>
+</ul>
+<ul>
+<li className="list-inline-item">
+  
+  <a href="/About"><div>About</div></a>
+</li>
+</ul>
+<ul>
+<li className="list-inline-item">
+  
+  <a href="/Hobbies"><div>Hobbies</div></a>
+</li>
+</ul>
+<ul>
+<li className="list-inline-item">
+  
+  <a href="/Contact"><div>Contact</div></a>
+</li>
+
+</ul>
+
+    </footer>
+    <p class="copyright">deen mohammad © 2023</p>
+    </div>
+    
+        
 )}
 
 export default Footer;
