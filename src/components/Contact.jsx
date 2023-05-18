25481265d69258e1c9a3de55762cea607c196f2c
deen <ilikemintyicecream@gmail.com>
2023-05-18 22:16:16 +0530

    final touch

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,25 +1,57 @@
 import contact from '../assets/contact.svg'
+import emailjs from '@emailjs/browser';
 import { useState, useRef } from 'react';
 import {styles} from '../styles';
 import '../index.css'
 
+
+
 const Contact = () => {
     const formRef = useRef();
-    const[form, setform] = useState({
+    const[form, setForm] = useState({
         name: '',
         email: '',
         message: '',
     });
 
-    const [loading, setloading] = useState(false);
+    const [loading, setLoading] = useState(false);
     const handleChange = (e) => {
         const{name, value}=e.target;
-        setform((prevState) => ({
-            ...prevState,
-            [name]:value,
-        }))
+        setForm({...form, [name]:value})
     }
-    const handleSubmit = (e) => {}
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setLoading(true);
+
+        emailjs.send(
+            'service id',
+            'template id',
+            {
+                from_name: form.name,
+                to_name: 'deen',
+                from_email: form.email,
+                to_email: 'deenusaifi8851@gmail.com',
+                message: form.message,
+            },
+            'public key'
+        )
+        .then(() => {
+            setLoading(false);
+            alert('Thank you, i will get back to you as soon as possible.');
+
+            setForm({
+                name: '',
+                email: '',
+                message: '',
+            })
+        },(error) => {
+            setLoading(false)
+
+            console.log(error);
+
+            alert('something went wrong.')
+        })
+    }
 
     return (
 
